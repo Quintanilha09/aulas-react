587432863fdb3895d2644f8d57c55d34fd6af711
EA - Gabriel Quintanilha Cabral De Oliveira <gabriel_quintanilha@educadventista.org>
2023-03-07 20:44:56 -0300

    primeiro commit

---
+++ src/components/Summary.jsx
@@ -0,0 +1,21 @@
+import { Rectangle } from "./Rectangle";
+
+import iconReaction from '../assets/icon-reaction.svg'
+import iconMemory from '../assets/icon-memory.svg'
+import iconVerbal from '../assets/icon-verbal.svg'
+import iconVisual from '../assets/icon-visual.svg'
+
+export function Summary() {
+    return (
+        <section>
+            <h2>Summary</h2>
+
+            <Rectangle icon={iconReaction} type={"Reaction"} number={80} />
+            <Rectangle icon = {iconMemory} type={"Memory"} number={92} />
+            <Rectangle type={"Verbal"} number={61} icon = {iconVerbal}/>
+            <Rectangle type={"Visual"} number={73} icon = {iconVisual}/>
+
+            <button>Continue</button>
+        </section>
+    )
+}
